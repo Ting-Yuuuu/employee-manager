8e367242802865711047a1d5cdda5928d72853eb
小村日和 <>
2026-05-18 17:34:56 +0800

    feat: 新增麵包屑組件 但未完成

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="breadcrumb_block">
+    <RouterLink to="/"  class="breadcrumb">首頁</RouterLink>
+    <a class="breadcrumb">></a>
+    <RouterLink to="/Employee_list" class="breadcrumb">全體員工資料表</RouterLink>
+    <a class="breadcrumb">></a>
+    <RouterLink to="/Edit_employee" class="breadcrumb">編輯員工資料</RouterLink>
+  </div>
+</template>
+
+<script setup>
+  import { useRoute, RouterLink } from 'vue-router';
+</script>
+
+<style lang="scss" scoped>
+ /* 麵包屑 */
+.breadcrumb_block{
+  display: flex;
+  gap: 8px;
+  margin: 16px 0;
+}
+
+.breadcrumb{
+  color: #333333;
+  font-size: 16px;
+  padding: 10px 0px;
+  text-decoration: none;
+}
+
+</style>
